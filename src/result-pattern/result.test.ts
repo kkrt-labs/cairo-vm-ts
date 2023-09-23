6845975f6e2c7a6d8105ad0334728eb339aa7572
Elias Tazartes <elias.tazartes@gmail.com>
2023-09-23 13:46:46 +0700

    feat: create result pattern

---
+++ src/result-pattern/result.test.ts
@@ -0,0 +1,71 @@
+import { test, expect, describe } from 'bun:test';
+import { Result } from './result';
+import { FeltError } from '../memory/primitives/felt';
+
+describe('Result', () => {
+  describe('success', () => {
+    test('should create a successful result', () => {
+      const result = Result.ok(42);
+      expect(result.isOk()).toBeTrue();
+      expect(result.unwrapOrUndefined()).toEqual(42);
+    });
+  });
+
+  describe('error', () => {
+    test('should create an error result', () => {
+      const error = new FeltError();
+      const result = Result.error(error);
+      expect(result.isErr()).toBeTrue();
+      expect(result.unwrapErrOrUndefined()).toBe(error);
+    });
+  });
+
+  describe('isOk', () => {
+    test('should correctly identify a successful result', () => {
+      const result = Result.ok('success');
+      expect(result.isOk()).toBeTrue();
+    });
+
+    test('should correctly identify an error result', () => {
+      const result = Result.error(new FeltError());
+      expect(result.isOk()).toBeFalse();
+    });
+  });
+
+  describe('isErr', () => {
+    test('should correctly identify an error result', () => {
+      const result = Result.error(new FeltError());
+      expect(result.isErr()).toBeTrue();
+    });
+
+    test('should correctly identify a successful result', () => {
+      const result = Result.ok('success');
+      expect(result.isErr()).toBeFalse();
+    });
+  });
+
+  describe('unwrapOrUndefined', () => {
+    test('should unwrap a successful result value', () => {
+      const result = Result.ok(123);
+      expect(result.unwrapOrUndefined()).toEqual(123);
+    });
+
+    test('should return undefined for an error result', () => {
+      const result = Result.error(new FeltError());
+      expect(result.unwrapOrUndefined()).toBeUndefined();
+    });
+  });
+
+  describe('unwrapErrOrUndefined', () => {
+    test('should unwrap an error result value', () => {
+      const error = new FeltError();
+      const result = Result.error(error);
+      expect(result.unwrapErrOrUndefined()).toBe(error);
+    });
+
+    test('should return undefined for a successful result', () => {
+      const result = Result.ok(123);
+      expect(result.unwrapErrOrUndefined()).toBeUndefined();
+    });
+  });
+});
