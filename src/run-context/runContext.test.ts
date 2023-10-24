8c85d1a80c62bda3a368adc87f561ac163adb039
Gregory Edison <gregory.edison1993@gmail.com>
2023-10-24 18:44:36 +0200

    fix tests

--- src/run-context/runContext.test.ts
+++ src/run-context/runContext.test.ts
@@ -28,6 +28,20 @@ describe('RunContext', () => {
   // https://github.com/lambdaclass/cairo-vm/blob/main/vm/src/vm/context/run_context.rs#L116
   describe('computeDstAddress', () => {
     test('should compute dst addr for ap register', () => {
+      const instruction: Instruction = {
+        offDst: SignedInteger16.toInt16(1),
+        offOp0: SignedInteger16.toInt16(2),
+        offOp1: SignedInteger16.toInt16(3),
+        dstReg: RegisterFlag.AP,
+        op0Reg: RegisterFlag.FP,
+        op1Src: Op1Src.AP,
+        resLogic: ResLogic.Add,
+        pcUpdate: PcUpdate.Regular,
+        apUpdate: ApUpdate.Regular,
+        fpUpdate: FpUpdate.Regular,
+        opcode: Opcode.NoOp,
+      };
+
       const runContext = new RunContext(4, 5, 6);
       const { value: offset } = SignedInteger16.toInt16(1);
 
