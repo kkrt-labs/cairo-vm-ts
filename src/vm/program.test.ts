f18f76308711d8a047bcda6d6b6017a628c51229
Gregory Edison <gregory.edison1993@gmail.com>
2023-11-16 12:24:45 +0300

    add deserialization of program

---
+++ src/vm/program.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, test } from 'bun:test';
+import * as fs from 'fs';
+import { parseProgram } from './program';
+import { Felt } from 'primitives/felt';
+
+describe('program', () => {
+  describe('parseProgram', () => {
+    test('should correctly parse the program data', () => {
+      const programContent = fs.readFileSync(
+        'cairo_programs/cairo_0/fibonacci.json',
+        'utf8'
+      );
+      const programJson = JSON.parse(programContent);
+      const data = programJson.data.map((element: string) => {
+        return new Felt(BigInt(element));
+      });
+
+      const program = parseProgram(programContent);
+      expect(program.data).toEqual(data);
+    });
+  });
+});
