8f9fe78f4305e6dac1ad7dc26f004232e8ade036
malatrax <simon.malatrait@grenoble-inp.org>
2024-05-29 09:53:12 +0200

    chore: remove unused CannotInferValue error

--- src/errors/builtins.ts
+++ src/errors/builtins.ts
@@ -1,16 +1,6 @@
 /** Errors related to builtins */
 export class BuiltinError extends Error {}
 
-/** Value cannot be infered from the given cell of the builtin segment */
-export class CannotInferValue extends BuiltinError {
-  public readonly offset: number;
-
-  constructor(offset: number) {
-    super();
-    this.offset = offset;
-  }
-}
-
 /** Value cannot be infered from undefined cell value */
 export class UndefinedValue extends BuiltinError {
   public readonly offset;
