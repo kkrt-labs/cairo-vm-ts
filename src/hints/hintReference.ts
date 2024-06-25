9dfa66a98a3df75cdc65021df239b130567bc3ea
malatrax <simon.malatrait@grenoble-inp.org>
2024-06-25 15:53:07 +0200

    doc: add JSDoc to HintReference

--- src/hints/hintReference.ts
+++ src/hints/hintReference.ts
@@ -4,12 +4,30 @@ import { Felt } from 'primitives/felt';
 import { Register } from 'vm/instruction';
 import { ApTrackingData, Reference } from 'vm/program';
 
+/**
+ * - `Immediate`: Should be read as a Felt (e.g. `42`)
+ * - `Reference`: Should be read as a Relocatable,
+ * can have an associated offset (e.g. `ap + 1`, `fp`)
+ * - `Value`: Should be read as an offset value
+ * to add or subtract from a Reference (e.g. `3`)
+ */
 export enum ValueType {
-  Value,
   Immediate,
   Reference,
+  Value,
 }
 
+/**
+ * OffsetValue is an object to conveniently represent
+ * the parsed offset of a reference
+ *
+ * Depending on the valueType, `immediate`,
+ * `reference` and/or`value` are assigned a value
+ *
+ * A `ValueType.Reference` value can be `dereferenced`,
+ * meaning that the value that will be used is the
+ * memory cell read at the computed address rather than the computed address.
+ */
 export type OffsetValue = {
   valueType: ValueType;
   dereferenced: boolean;
@@ -18,6 +36,31 @@ export type OffsetValue = {
   register?: Register;
 };
 
+/**
+ * Parse a `reference` from the compilation artifact
+ * and output it in an exploitable format
+ *
+ * A reference follows the general pattern
+ * `cast(Offset1 + Offset2, valueType)`
+ * or `[cast(Offset1 + Offset2, valueType)]` if dereferenced
+ *
+ * - `valueType`: the reference value type (e.g. `felt*` in `cast(ap + 1, felt*)`)
+ * - `dereferenced`: flag whether the return value is
+ * the memory read at the computed address or the computed address
+ *
+ * Example with `ap + 1 = Relocatable(0, 2)` and `[ap + 1] = 10`
+ * - `cast(ap + 1, felt*)` returns `Relocatable(0, 2)`
+ * - `[cast(ap + 1, felt)]` returns `Felt(10n)`
+ *
+ * - `apTrackingData`: The correction to apply if one
+ * of the OffsetValue is a reference to AP.
+ * It is needed because AP changes at almost each instruction,
+ * thus the value of AP between the variable assignment
+ * and the hint execution may vary.
+ *
+ * - `offset1` The first OffsetValue.
+ * - `offset2` The second OffsetValue. It cannot be an Immediate
+ */
 export class HintReference {
   constructor(
     public valueType: string,
@@ -33,7 +76,26 @@ export class HintReference {
     this.offset2 = offset2;
   }
 
-  static parseReference(reference: Reference) {
+  /**
+   * Parse a reference string from the compilation artifact to an object
+   *
+   * @param reference The reference from the compilation artifact to be parsed
+   * @returns {HintReference} A HintReference object later used in the IdsManager to execute hints
+   *
+   * NOTE: Reference pattern
+   * - `cast(offset1 + offset2)`
+   * - `[cast(offset1 + offset2)]`
+   *
+   * with `offset1`: `immediate`, `reg1 + value1` or `[reg1 + value1]`
+   *
+   * with `offset2`: `undefined`, `reg2 + value2`, `[reg2 + value2]` or `value2`
+   *
+   * `offset2` is always undefined is `offset1` is `immediate` (e.g. `cast(42, felt)`)
+   *
+   * `value1` and `value2` are relative integers. If negative,
+   * they are wrapped into parenthesis (e.g. `cast(ap + (-1), felt*)`)
+   */
+  static parseReference(reference: Reference): HintReference {
     const dereferenced =
       reference.value.startsWith('[') && reference.value.endsWith(']');
     const ref = dereferenced ? reference.value.slice(1, -1) : reference.value;
@@ -70,7 +132,7 @@ export class HintReference {
           .split('+')
           .map((str) => str.trim())
       : offsetStr1.split('+').map((str) => str.trim());
-    // cast(fp - 2 + 5, felt*)
+    // Handle special case `cast(reg - value1 + value2)`
     if (off1Array[0] !== 'fp' && off1Array[0] !== 'ap') {
       const off1SubExpr = off1Array[0].split('-').map((str) => str.trim());
       if (
@@ -102,7 +164,7 @@ export class HintReference {
         offset2
       );
     }
-    // cast(fp + (-2), felt*)
+
     let off1: number = 0;
     if (off1Array[1]) {
       const value =
