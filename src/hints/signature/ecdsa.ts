2676b417db30acb01e7b04f5ad767feca68146d5
malatrax <simon.malatrait@grenoble-inp.org>
2024-06-25 12:17:36 +0200

    doc: add ecdsa add signature JSDoc

--- src/hints/signature/ecdsa.ts
+++ src/hints/signature/ecdsa.ts
@@ -5,6 +5,15 @@ import { VirtualMachine } from 'vm/virtualMachine';
 import { EcdsaSegment } from 'builtins/ecdsa';
 import { IdsManager } from 'hints/idsManager';
 
+/**
+ * Hint to add an ECDSA signature to the
+ * `signatures` array of the ECDSA builtin
+ *
+ * Function linked to `ADD_ECDSA_SIGNATURE`:
+ * ```python
+ * %{ecdsa_builtin.add_signature(ids.ecdsa_ptr.address_, (ids.signature_r, ids.signature_s))}
+ * ```
+ */
 export function addECDSASignature(ids: IdsManager, vm: VirtualMachine) {
   const r = ids.get('signature_r', vm);
   const s = ids.get('signature_s', vm);
