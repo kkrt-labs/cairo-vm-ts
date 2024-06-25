ac4015df146191fabc73370617743360cf75ecca
malatrax <simon.malatrait@grenoble-inp.org>
2024-06-25 12:17:56 +0200

    doc: add HintProcessor JSDoc

--- src/hints/hint.ts
+++ src/hints/hint.ts
@@ -20,6 +20,12 @@ export class HintProcessor {
     this.scopeManager = new ScopeManager();
   }
 
+  /** Compile an hint from the compilation artifact
+   * to a format that can be easily executed.
+   *
+   * @param hint The hint to be compiled, from the compilation
+   * @param refManager All the program references
+   */
   static compile(hint: Hint, refManager: ReferenceManager): HintData {
     const references = new Map<string, HintReference>();
     Object.entries(hint.flow_tracking_data.reference_ids).map(
@@ -40,6 +46,18 @@ export class HintProcessor {
     };
   }
 
+  /**
+   * Execute an hint based on its code
+   * `hint.code` allows mapping the Python hint code
+   * to its associated function in TypeScript
+   *
+   * `hint.ids` and `vm` provide the context
+   * on which the hint must be executed.
+   *
+   * The virtual machine stores the constants
+   * used by the program.
+   *
+   */
   execute(hint: HintData, vm: VirtualMachine) {
     switch (hint.code) {
       case hintCode.ADD_ECDSA_SIGNATURE:
