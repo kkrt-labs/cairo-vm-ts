fbd9210e8dd8c39de2a265915ad26d4ea33c3802
malatrax <simon.malatrait@grenoble-inp.org>
2024-07-23 17:43:38 +0200

    doc: add JSDoc to SquashedDictManager

--- src/hints/squashedDictManager.ts
+++ src/hints/squashedDictManager.ts
@@ -6,8 +6,13 @@ import {
 
 import { Felt } from 'primitives/felt';
 
+/**
+ *  Handle the squashing of dictionnaries.
+ */
 export class SquashedDictManager {
+  /** Maps the key of a dictionnary to its taken values accross the run. */
   public keyToIndices: Map<string, Felt[]>;
+  /** An array containing the keys that still needs to be squashed. */
   public keys: Felt[];
 
   constructor() {
@@ -15,6 +20,7 @@ export class SquashedDictManager {
     this.keys = [];
   }
 
+  /** Add `index` to the indices taken by `key` */
   insert(key: Felt, index: Felt) {
     const keyStr = key.toString();
     const indices = this.keyToIndices.get(keyStr);
@@ -25,18 +31,21 @@ export class SquashedDictManager {
     }
   }
 
+  /** Return the last key of the dictionnary. */
   lastKey(): Felt {
     const len = this.keys.length;
     if (!len) throw new EmptyKeys();
     return this.keys[len - 1];
   }
 
+  /** Remove and return the last key of the dictionnary. */
   popKey(): Felt {
     const key = this.keys.pop();
     if (!key) throw new EmptyKeys();
     return key;
   }
 
+  /** Return the array of indices taken by the last key. */
   lastIndices(): Felt[] {
     const key = this.lastKey();
     const indices = this.keyToIndices.get(key.toString());
@@ -44,6 +53,7 @@ export class SquashedDictManager {
     return indices;
   }
 
+  /** Return the last index of the indices taken by the last key. */
   lastIndex(): Felt {
     const indices = this.lastIndices();
     const len = indices.length;
@@ -51,6 +61,7 @@ export class SquashedDictManager {
     return indices[len - 1];
   }
 
+  /** Remove and return the last index of the indices taken by the last key. */
   popIndex(): Felt {
     const index = this.lastIndices().pop();
     if (!index) throw new EmptyIndex();
