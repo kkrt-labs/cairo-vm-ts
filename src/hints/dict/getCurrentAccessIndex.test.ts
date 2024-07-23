04670e7deeccbdc6f46a27734fc26ec279e0e543
malatrax <simon.malatrait@grenoble-inp.org>
2024-07-23 15:14:44 +0200

    feat: add unit test GetCurrentAccessIndex

---
+++ src/hints/dict/getCurrentAccessIndex.test.ts
@@ -0,0 +1,55 @@
+import { describe, expect, test } from 'bun:test';
+
+import { Register } from 'vm/instruction';
+import { VirtualMachine } from 'vm/virtualMachine';
+import { HintName } from 'hints/hintName';
+import { Felt } from 'primitives/felt';
+import { getCurrentAccessIndexParser } from './getCurrentAccessIndex';
+import { OpType } from 'hints/hintParamsSchema';
+import { rangeCheckHandler } from 'builtins/rangeCheck';
+
+const GET_CURRENT_ACCESS_INDEX = {
+  GetCurrentAccessIndex: {
+    range_check_ptr: {
+      Deref: {
+        register: 'AP',
+        offset: 0,
+      },
+    },
+  },
+};
+
+describe('GetCurrentAccessIndex', () => {
+  test('should properly parse GetCurrentAccessIndex hint', () => {
+    const hint = getCurrentAccessIndexParser.parse(GET_CURRENT_ACCESS_INDEX);
+    expect(hint).toEqual({
+      type: HintName.GetCurrentAccessIndex,
+      rangeCheckPtr: {
+        type: OpType.Deref,
+        cell: {
+          register: Register.Ap,
+          offset: 0,
+        },
+      },
+    });
+  });
+
+  test('should properly execute GetCurrentAccessIndex hint', () => {
+    const hint = getCurrentAccessIndexParser.parse(GET_CURRENT_ACCESS_INDEX);
+    const vm = new VirtualMachine();
+    vm.memory.addSegment();
+    vm.memory.addSegment();
+    const rangeCheck = vm.memory.addSegment(rangeCheckHandler(128n));
+
+    vm.memory.assertEq(vm.ap, rangeCheck);
+
+    const key = new Felt(0n);
+    const indices = [new Felt(6n), new Felt(3n), new Felt(35n)];
+    vm.squashedDictManager.keyToIndices.set(key.toString(), indices);
+    vm.squashedDictManager.keys = [key];
+
+    vm.executeHint(hint);
+
+    expect(vm.memory.get(rangeCheck)).toEqual(indices[indices.length - 1]);
+  });
+});
