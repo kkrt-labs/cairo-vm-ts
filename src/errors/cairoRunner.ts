6bdf9d31521ca117f7b4daceb34fb14de2fae611
malatrax <simon.malatrait@grenoble-inp.org>
2024-07-25 10:52:29 +0200

    chore: add JSDoc CairoRunner errors

--- src/errors/cairoRunner.ts
+++ src/errors/cairoRunner.ts
@@ -1,23 +1,27 @@
 class CairoRunnerError extends Error {}
 
+/** The relocated memory is empty. It cannot be exported. */
 export class EmptyRelocatedMemory extends CairoRunnerError {
   constructor() {
     super('Relocated memory is empty');
   }
 }
 
+/** The Cairo Zero hints are not supported. */
 export class CairoZeroHintsNotSupported extends CairoRunnerError {
   constructor() {
     super('Cairo Zero hints are not supported yet.');
   }
 }
 
+/** The output serialization of Cairo programs is not supported. */
 export class CairoOutputNotSupported extends CairoRunnerError {
   constructor() {
     super('The output serialization of Cairo programs is not supported yet.');
   }
 }
 
+/** The given entrypoint is not in the program, it cannot be executed. */
 export class UndefinedEntrypoint extends CairoRunnerError {
   constructor(name: string) {
     super(`The function to be executed doesn't exist: ${name}`);
