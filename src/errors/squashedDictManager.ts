4a12cdf12891edc5bf1cc150e08830fa5f9b364c
Malatrax <simon.malatrait@grenoble-inp.org>
2024-07-30 14:03:57 +0200

    chore: typo

--- src/errors/squashedDictManager.ts
+++ src/errors/squashedDictManager.ts
@@ -2,18 +2,18 @@ import { Felt } from 'primitives/felt';
 
 class SquashedDictManagerError extends Error {}
 
-/** There is no keys left in the squashed dictionnary manager */
+/** There are no keys left in the squashed dictionnary manager */
 export class EmptyKeys extends SquashedDictManagerError {
   constructor() {
-    super('There is no keys left in the squashed dictionnary manager.');
+    super('There are no keys left in the squashed dictionnary manager.');
   }
 }
 
-/** There is no indices at `key` */
+/** There are no indices at `key` */
 export class EmptyIndices extends SquashedDictManagerError {
   constructor(key: Felt | undefined) {
     super(
-      `There is no indices at key ${
+      `There are no indices at key ${
         key ? key.toString() : key
       } in the squashed dictionnary manager.`
     );
